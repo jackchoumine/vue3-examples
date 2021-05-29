7a2ead3cabb90da9f9949a3f14cfd69d52ac4c86
jackchou <jackzhou4job@163.com>
2021-05-29 12:45:20 +0800

    render 基础

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,70 @@
 <template>
-  <dir>
+  <div>
     <h1>vue3 render exmaple</h1>
+    <MyButtonRender title="title" @my-click="onMyClick">
+      <template v-slot:left>
+        <span>左边插槽</span>
+      </template>
+      <template v-slot="{ person }">
+        <span>默认插槽{{ person.age }}</span>
+      </template>
+      <template v-slot:right="{ age }">
+        <span>右边插槽{{ age }}</span>
+      </template>
+    </MyButtonRender>
+    <MyButtonSetup title="title">
+      <template v-slot:left>
+        <span>左边插槽</span>
+      </template>
+      <template v-slot="{ person }">
+        <span>默认插槽{{ person.age }}</span>
+      </template>
+      <template v-slot:right="{ age }">
+        <span>右边插槽{{ age }}</span>
+      </template>
+    </MyButtonSetup>
+    <MyButton title="title">
+      <template v-slot:left>
+        <span>左边插槽</span>
+      </template>
+      <template v-slot="{ person }">
+        <span>默认插槽{{ person.age }}</span>
+      </template>
+      <template v-slot:right="{ age }">
+        <span>右边插槽{{ age }}</span>
+      </template>
+    </MyButton>
     <MyHead :h="1" class="h1" />
     <MyHead :h="2" style="color: red" />
     <MyHead :h="3" id="h3" />
     <MyHead :h="4" data-key="my-key" />
     <MyHead :h="5" data-key="my-key" @my-event="onMyEvent">
-      <template #default>
+      <template v-slot:default>
         <button>默认插槽</button>
       </template>
     </MyHead>
     <p>data form child:{{ dataFromChild }}</p>
     <hr />
     <button @click="changeSlot">修改YouHead插槽</button>
-    <YouHead :h="1" @my-click="onMyClick">
+    <YouHead :h="1" @my-click="onMyClick" title="title">
       <span>默认插槽{{ dataFromChild }}</span>
+      <template v-slot:right>
+        <span>右边</span>
+      </template>
     </YouHead>
-  </dir>
+  </div>
 </template>
 
 <script>
 import { ref } from 'vue'
+import MyButton from './components/MyButton.vue'
 import MyHead from './components/MyHead.vue'
 import YouHead from './components/YouHead.jsx'
+import MyButtonSetup from './components/MyButton.jsx'
+import MyButtonRender from './components/MyButtonRender'
 export default {
   name: 'App',
-  components: { MyHead, YouHead },
+  components: { MyButtonRender, MyButtonSetup, MyButton, MyHead, YouHead },
   setup() {
     const dataFromChild = ref(Math.random().toString(36))
     const onMyEvent = data => {
--- src/components/YouHead.jsx
+++ src/components/YouHead.jsx
@@ -1,33 +1,42 @@
-import { h, ref, reactive, watch, computed } from 'vue'
+import { ref } from 'vue'
 export default {
   name: 'YouHead',
   inheritAttrs: false,
   props: ['h'],
-  setup(props, { slots, emit }) {
+  setup(props, { slots, emit, attrs }) {
+    console.log('setup props')
+    console.log(props)
+    console.log('setup slots')
+    console.log(slots) // is real slots
+    console.log('setup attrs')
+    console.log(attrs) // contains event and props
     const person = ref({ name: 'jack', age: 23 })
-
-    //NOTE 可监听插槽的变化
-    // 神奇
-    // watch(
-    //   () => slots.default(),
-    //   value => {
-    //     console.log(value)
-    //   }
-    // )
+    //   const tech = ref('vue')
 
     return {
+      // tech,
       person,
-      // 在这里提供默认插槽
-      slots,
-      emit,
+      // slots,
+      // emit,
     }
   },
   render() {
+    console.log('props')
+    console.log(this.$props) // not props
+    console.log('slots')
+    console.log(this.$slots) // [] not slots
+    // console.log(this.$scopedSlots);// 没有作用域插槽了
+    console.log('attrs')
+    console.log(this.$attrs) // { h:1}
+    console.log('vnode')
+    // console.log(this.$vnode)
     const handlerClick = () => {
-      this.emit('my-click', this.person.age)
+      this.person.age = Math.random().toString(36)
+      this.$emit('my-click', this.person.age)
     }
-    const button = <button onClick={handlerClick}>{this.slots.default()}</button>
-    const children = [this.person.name, button]
-    return h(`h1`, null, children)
+    const button = <button onClick={handlerClick}>{this.$slots.default && this.$slots.default()}</button>
+    const rightSlot = this.$slots?.right()
+    const children = [this.person.age, button, rightSlot]
+    return <h1>{children}</h1>
   },
 }
